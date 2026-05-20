d6653905f7a400253fea9c24047ff876792dbc43
RBVC <sacra.motimoti+RVC@gmail.com>
2026-05-20 10:24:30 +0900

    RBVC/TVT/group.js

---
+++ group.js
@@ -0,0 +1,32 @@
+// group.js
+const allGroups = {
+    "gen1us": {
+        name: "GEN1US",
+        concept: "Masquerade",
+        banner: "images/gen1us/masquerade-gen1us.jpg",
+        members: [
+            { name: "Diluc", div: "Mondstadt", pos: "Main Vocal / Lead Dancer", img: "images/gen1us/masquerade-diluc.jpg" },
+            { name: "Ayato", div: "Inazuma", pos: "Visual / Lead Vocal", img: "images/gen1us/masquerade-ayato.jpg" },
+            { name: "Zhongli", div: "Liyue", pos: "Leader / Lead Rapper", img: "images/gen1us/masquerade-zhongli.jpg" },
+            { name: "Tartaglia", div: "Liyue", pos: "Main Dancer / Lead Rapper", img: "images/gen1us/masquerade-tartaglia.jpg" },
+            { name: "Kaeya", div: "Mondstadt", pos: "Main Vocal", img: "images/gen1us/masquerade-kaeya.jpg" },
+            { name: "Baizhu", div: "Liyue", pos: "Main Vocal", img: "images/gen1us/masquerade-baizhu.jpg" },
+            { name: "Itto", div: "Inazuma", pos: "Main Rapper", img: "images/gen1us/masquerade-itto.jpg" },
+            { name: "Dainsleif", div: "Mondstadt", pos: "Main Dancer / Sub Rapper", img: "images/gen1us/masquerade-dainsleif.jpg" },
+            { name: "Thoma", div: "Inazuma", pos: "Main Dancer / Sub Vocal", img: "images/gen1us/masquerade-thoma.jpg" }
+        ],
+        discography: [
+            { title: "Masquerade", type: "1st Single Album", date: "202X.05.20", embed: "" }
+        ]
+    },
+    "6reeze": {
+        name: "6reeze",
+        concept: "Venti-lation",
+        banner: "images/6reeze/main.jpg",
+        members: [
+            { name: "Venti", div: "Mondstadt", pos: "Main Vocal / Center", img: "images/6reeze/venti.jpg" }
+        ],
+        discography: []
+    }
+    // 今後 4ggravate 等もここに追加
+};
