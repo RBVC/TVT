8c330eea91f6e3bd4fb06dbce51e966745f02a4d
RBVC <sacra.motimoti+RVC@gmail.com>
2026-05-20 11:26:41 +0900

    0520

--- group.js
+++ group.js
@@ -5,15 +5,15 @@ const allGroups = {
         concept: "Masquerade",
         banner: "images/gen1us/masquerade-gen1us.jpg",
         members: [
-            { name: "Diluc", div: "Mondstadt", pos: "Main Vocal\nLead Dancer", img: "images/gen1us/masquerade-diluc.jpg" },
-            { name: "Zhongli", div: "Liyue", pos: "Lead Rapper\nSub Vocal", img: "images/gen1us/masquerade-zhongli.jpg" },
+            { name: "Diluc", div: "Mondstadt", pos: "Main Vocal / Lead Dancer", img: "images/gen1us/masquerade-diluc.jpg" },
+            { name: "Zhongli", div: "Liyue", pos: "Lead Rapper / Sub Vocal", img: "images/gen1us/masquerade-zhongli.jpg" },
             { name: "Itto", div: "Inazuma", pos: "Main Rapper", img: "images/gen1us/masquerade-itto.jpg" },
-            { name: "Baizhu", div: "Liyue", pos: "Leader\nMain Vocal", img: "images/gen1us/masquerade-baizhu.jpg" },
-            { name: "Tartaglia", div: "Liyue & Snezhnaya", pos: "Lead Dancer\nLead Rapper", img: "images/gen1us/masquerade-tartaglia.jpg" },
-            { name: "Ayato", div: "Inazuma", pos: "Visual\nLead Vocal", img: "images/gen1us/masquerade-ayato.jpg" },
+            { name: "Baizhu", div: "Liyue", pos: "Leader / Main Vocal", img: "images/gen1us/masquerade-baizhu.jpg" },
+            { name: "Tartaglia", div: "Liyue & Snezhnaya", pos: "Lead Dancer / Lead Rapper", img: "images/gen1us/masquerade-tartaglia.jpg" },
+            { name: "Ayato", div: "Inazuma", pos: "Visual / Lead Vocal", img: "images/gen1us/masquerade-ayato.jpg" },
             { name: "Kaeya", div: "Mondstadt", pos: "Main Vocal", img: "images/gen1us/masquerade-kaeya.jpg" },
-            { name: "Dainsleif", div: "Mondstadt", pos: "Main Dancer\nSub Rapper", img: "images/gen1us/masquerade-dainsleif.jpg" },
-            { name: "Thoma", div: "Inazuma", pos: "Main Dancer\nSub Vocal", img: "images/gen1us/masquerade-thoma.jpg" }
+            { name: "Dainsleif", div: "Mondstadt", pos: "Main Dancer / Sub Rapper", img: "images/gen1us/masquerade-dainsleif.jpg" },
+            { name: "Thoma", div: "Inazuma", pos: "Main Dancer / Sub Vocal", img: "images/gen1us/masquerade-thoma.jpg" }
         ],
         discography: [
             { title: "Masquerade", type: "1st Single Album", date: "202X.05.20", embed: "" }
