7f7c911a36c64ee90df46a8be773b8d120fae767
RBVC <sacra.motimoti+RVC@gmail.com>
2026-05-21 19:40:43 +0900

    up

--- group.js
+++ group.js
@@ -3,6 +3,7 @@ const allGroups = {
     "gen1us": {
         name: "GEN1US",
         concept: "MASQUERADE",
+        layout: 3, // 特殊改行：PCで4列サイズ、3人ごとに改行
         banner: "images/gen1us/masquerade-gen1us.jpg",
         members: [
             { name: "Diluc", div: "Mondstadt", pos: "Main Vocal / Lead Dancer", img: "images/gen1us/masquerade-diluc.jpg" },
@@ -15,13 +16,12 @@ const allGroups = {
             { name: "Dainsleif", div: "Mondstadt", pos: "Main Dancer / Sub Rapper", img: "images/gen1us/masquerade-dainsleif.jpg" },
             { name: "Thoma", div: "Inazuma", pos: "Main Dancer / Sub Vocal", img: "images/gen1us/masquerade-thoma.jpg" }
         ],
-        discography: [
-            { title: "MASQUERADE", type: "1st Single Album", date: "202X.05.20", embed: "" }
-        ]
+        discography: [{ title: "MASQUERADE", type: "1st Single Album", date: "202X.05.20" }]
     },
     "4ggravate": {
         name: "4ggravate",
         concept: "CYBER / HIPHOP",
+        layout: 4, // 4人組なので4列でパシッと
         banner: "images/4ggravate/4ggravate.jpg",
         members: [
             { name: "Alhaitham", div: "Sumeru", pos: "Main Dancer / Lead Rapper / Sub Vocal", img: "images/4ggravate/cyber-alhaitham.jpg" },
@@ -34,8 +34,16 @@ const allGroups = {
     "6reeze": {
         name: "6reeze",
         concept: "VENTI-LATION",
+        layout: 3, // 6人組。3人×2行で綺麗に
         banner: "",
-        members: [],
+        members: [
+            { name: "Venti", div: "Mondstadt", pos: "Main Vocal / Center", img: "" },
+            { name: "Xiao", div: "Liyue", pos: "Lead Rapper / Sub Vocal", img: "" },
+            { name: "Kazuha", div: "Inazuma", pos: "Lead Vocal / Lead Dancer", img: "" },
+            { name: "Heizou", div: "Inazuma", pos: "Main Rapper / Lead Dancer", img: "" },
+            { name: "Wanderer", div: "Sumeru", pos: "Main Dancer / Sub Vocal", img: "" },
+            { name: "Aether", div: "Sumeru", pos: "Main Vocal / Leader", img: "" }
+        ],
         discography: []
     }
 };
