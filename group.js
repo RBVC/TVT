3bac1584bab4398d8e8051d15e449ea4d9b5d1c3
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-13 16:27:42 +0900

    UP

--- group.js
+++ group.js
@@ -6,15 +6,15 @@ const allGroups = {
         layout: 3,
         banner: "images/gen1us/masquerade-gen1us.jpg",
         members: [
-            { name: "Diluc", div: "Mondstadt", pos: "Main Vocal / Lead Dancer", img: "images/gen1us/masquerade-diluc.jpg" },
+            { name: "Diluc", div: "Mondstadt", pos: "Main Vocal / Sub Dancer", img: "images/gen1us/masquerade-diluc.jpg" },
             { name: "Zhongli", div: "Liyue", pos: "Lead Rapper / Sub Vocal", img: "images/gen1us/masquerade-zhongli.jpg" },
             { name: "Itto", div: "Inazuma", pos: "Main Rapper", img: "images/gen1us/masquerade-itto.jpg" },
             { name: "Baizhu", div: "Liyue", pos: "Leader / Main Vocal", img: "images/gen1us/masquerade-baizhu.jpg" },
-            { name: "Tartaglia", div: "Liyue & Snezhnaya", pos: "Lead Dancer / Lead Rapper", img: "images/gen1us/masquerade-tartaglia.jpg" },
+            { name: "Tartaglia", div: "Snezhnaya", pos: "Main Dancer / Lead Rapper", img: "images/gen1us/masquerade-tartaglia.jpg" },
             { name: "Ayato", div: "Inazuma", pos: "Visual / Lead Vocal", img: "images/gen1us/masquerade-ayato.jpg" },
             { name: "Kaeya", div: "Mondstadt", pos: "Main Vocal", img: "images/gen1us/masquerade-kaeya.jpg" },
             { name: "Dainsleif", div: "Mondstadt", pos: "Main Dancer / Sub Rapper", img: "images/gen1us/masquerade-dainsleif.jpg" },
-            { name: "Thoma", div: "Inazuma", pos: "Main Dancer / Sub Vocal", img: "images/gen1us/masquerade-thoma.jpg" }
+            { name: "Thoma", div: "Inazuma", pos: "Lead Dancer / Sub Vocal", img: "images/gen1us/masquerade-thoma.jpg" }
         ],
         discography: []
     },
@@ -24,10 +24,10 @@ const allGroups = {
         layout: 4,
         banner: "images/4ggravate/4ggravate.jpg",
         members: [
-            { name: "Alhaitham", div: "Sumeru", pos: "Main Dancer / Lead Rapper / Sub Vocal", img: "images/4ggravate/cyber-alhaitham.jpg" },
-            { name: "Cyno", div: "Sumeru", pos: "Main Rapper / Sub Vocal / Sub Dancer", img: "images/4ggravate/cyber-cyno.jpg" },
-            { name: "Kaveh", div: "Sumeru", pos: "Lead Dancer / Lead Vocal / Sub Rapper", img: "images/4ggravate/cyber-kaveh.jpg" },
-            { name: "Tighnari", div: "Sumeru", pos: "Leader / Main Vocal / Lead Dancer / Sub Rapper", img: "images/4ggravate/cyber-tighnari.jpg" }
+            { name: "Alhaitham", div: "Sumeru", pos: "Main Dancer / Lead Rapper", img: "images/4ggravate/cyber-alhaitham.jpg" },
+            { name: "Cyno", div: "Sumeru", pos: "Main Rapper", img: "images/4ggravate/cyber-cyno.jpg" },
+            { name: "Kaveh", div: "Sumeru", pos: "Lead Dancer / Lead Vocal", img: "images/4ggravate/cyber-kaveh.jpg" },
+            { name: "Tighnari", div: "Sumeru", pos: "Leader / Main Vocal", img: "images/4ggravate/cyber-tighnari.jpg" }
         ],
         discography: [
             {
@@ -39,15 +39,15 @@ const allGroups = {
     },
     "6reeze": {
         name: "6reeze",
-        concept: "Kawaii / Cool",
+        concept: "EMPATHY",
         layout: 3,
         banner: "images/6reeze/6reeze.jpg",
         members: [
-            { name: "Wanderer", div: "Sumeru", pos: "Main Dancer / Lead Vocal / Sub Rapper", img: "images/6reeze/heart-wanderer.jpg" },
+            { name: "Wanderer", div: "Inazuma", pos: "Main Dancer / Sub Vocal / Sub Rapper", img: "images/6reeze/heart-wanderer.jpg" },
             { name: "Xiao", div: "Liyue", pos: "Main Rapper / Sub Vocal", img: "images/6reeze/heart-xiao.jpg" },
             { name: "Venti", div: "Mondstadt", pos: "Main Vocal / Center", img: "images/6reeze/heart-venti.jpg" },
             { name: "Kazuha", div: "Inazuma", pos: "Main Vocal / Lead Dancer", img: "images/6reeze/heart-kazuha.jpg" },
-            { name: "Aether", div: "???", pos: "Lead Vocal / Leader", img: "images/6reeze/heart-aether.jpg" },
+            { name: "Aether", div: "???", pos: "Leader / Lead Vocal", img: "images/6reeze/heart-aether.jpg" },
             { name: "Heizou", div: "Inazuma", pos: "Lead Rapper / Lead Dancer", img: "images/6reeze/heart-heizou.jpg" }
         ],
         discography: [
@@ -60,13 +60,13 @@ const allGroups = {
     },
     "universe": {
         name: "TVT U",
-        concept: "Universe / Dark / Mysterious",
+        concept: "HANA",
         layout: 4,
         banner: "images/universe/tvt-u.jpg",
         members: [
             { name: "Gaming", div: "Liyue", pos: "Main Vocal", img: "images/universe/universe-gaming.jpg" },
             { name: "Chongyun", div: "Liyue", pos: "Lead Rapper", img: "images/universe/universe-chongyun.jpg" },
-            { name: "Xingqiu", div: "Liyue", pos: "Main Vocal", img: "images/universe/universe-xingqiu.jpg" },
+            { name: "Xingqiu", div: "Liyue", pos: "Leader / Main Vocal", img: "images/universe/universe-xingqiu.jpg" },
             { name: "Albedo", div: "Mondstadt", pos: "Sub Vocal", img: "images/universe/universe-albedo.jpg" },
             { name: "Gorou", div: "Inazuma", pos: "Lead Dancer", img: "images/universe/universe-gorou.jpg" },
             { name: "Bennett", div: "Mondstadt", pos: "Main Dancer", img: "images/universe/universe-bennett.jpg" },
