244201b2ef28924d70bc07d2c7906d8e6193a736
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-18 18:07:27 +0900

    up

--- group.js
+++ group.js
@@ -1,6 +1,46 @@
 // group.js - Teyvat Entertainment Master Database
 
-// HEAVENTEEN 順位データ (Wiki自動生成用)
+// SPECIAL PROJECTS (TEYVAT TIMES / news.html 用)
+const specialProjects = {
+    "mafia-vs-police": {
+        title: "Mafia vs Police",
+        tagline: "The Chessboard of Teyvat: Who truly moves the pieces?",
+        mainVisual: "images/mafia_vs_police/main.jpg",
+        description: "裏社会を支配する『Mafia』と、秩序を守る精鋭『Police』。二つの勢力が激突する、Teyvat Entertainment 史上最大のスペシャルプロジェクト。表向きの総監はヌヴィレットだが、Policeの真の支配者は別にいるという噂も……。",
+        teams: [
+            {
+                name: "Team Mafia",
+                model: "Stray Kids",
+                members: [
+                    { name: "Zhongli", role: "THE BOSS", img: "images/mafia_vs_police/mafia/zhongli.jpg" },
+                    { name: "Ayato", role: "THE UNDERBOSS", img: "images/mafia_vs_police/mafia/ayato.jpg" },
+                    { name: "Xiao", role: "RIGHT-HAND MAN", img: "images/mafia_vs_police/mafia/xiao.jpg" },
+                    { name: "Tartaglia", role: "MERCENARY", img: "images/mafia_vs_police/mafia/tartaglia.jpg" },
+                    { name: "Wanderer", role: "THE HEIR", img: "images/mafia_vs_police/mafia/wanderer.jpg" },
+                    { name: "Kaeya", role: "THE CONSIGLIERE", img: "images/mafia_vs_police/mafia/kaeya.jpg" },
+                    { name: "Kazuha", role: "THE BODYGUARD", img: "images/mafia_vs_police/mafia/kazuha.jpg" },
+                    { name: "Wriothesley", role: "UNDERCOVER AGENT", img: "images/mafia_vs_police/mafia/wriothesley.jpg" }
+                ]
+            },
+            {
+                name: "Team Police",
+                model: "THE BOYZ",
+                members: [
+                    { name: "Venti", role: "THE CASANOVA (True Mastermind)", img: "images/mafia_vs_police/police/venti.jpg" },
+                    { name: "Neuvillette", role: "CHIEF JUSTICE", img: "images/mafia_vs_police/police/neuvillette.jpg" },
+                    { name: "Alhaitham", role: "CHIEF COMMANDER", img: "images/mafia_vs_police/police/alhaitham.jpg" },
+                    { name: "Diluc", role: "THE VIGILANTE", img: "images/mafia_vs_police/police/diluc.jpg" },
+                    { name: "Baizhu", role: "THE PATHOLOGIST", img: "images/mafia_vs_police/police/baizhu.jpg" },
+                    { name: "Cyno", role: "TACTICAL POLICE", img: "images/mafia_vs_police/police/cyno.jpg" },
+                    { name: "Tighnari", role: "THE SCIENTIST", img: "images/mafia_vs_police/police/tighnari.jpg" },
+                    { name: "Kaveh", role: "THE DETECTIVE", img: "images/mafia_vs_police/police/kaveh.jpg" }
+                ]
+            }
+        ]
+    }
+};
+
+// ...以下の heaventeenResults, labels, allGroups は既存のデータを維持...
 const heaventeenResults = [
     { name: "Tartaglia", rank0: 9, rank1: 4, rank2: 3, final: 1, result: "GEN1US" },
     { name: "Dainsleif", rank0: 1, rank1: 2, rank2: 4, final: 2, result: "GEN1US" },
@@ -13,13 +53,12 @@ const heaventeenResults = [
     { name: "Itto", rank0: 13, rank1: 11, rank2: 7, final: 9, result: "GEN1US" },
     { name: "Alhaitham", rank0: 3, rank1: 8, rank2: 99, final: 99, result: "4ggravate" },
     { name: "Kaveh", rank0: 10, rank1: 9, rank2: 99, final: 99, result: "4ggravate" },
-    { name: "Neuvillette", rank0: 5, rank1: 99, rank2: 99, final: 99, result: "House of the Hearth" },
-    { name: "Wriothesley", rank0: 6, rank1: 99, rank2: 99, final: 99, result: "House of the Hearth" }
+    { name: "Neuvillette", rank0: 5, rank1: 99, rank2: 99, final: 99, result: "H.O.T.H" },
+    { name: "Wriothesley", rank0: 6, rank1: 99, rank2: 99, final: 99, result: "H.O.T.H" }
 ];
 
-// レーベル情報
 const labels = {
-    "HQ": { name: "TEYVAT ENT. (HQ)", ceo: "パイモン" }, // ✅ RBVCから変更
+    "HQ": { name: "TEYVAT ENT. (HQ)", ceo: "パイモン" },
     "FAVONIUS": { name: "FAVONIUS MUSIC", ceo: "ジン" },
     "QIXING": { name: "QIXING STUDIO", ceo: "凝光" },
     "NARUKAMI": { name: "NARUKAMI RECORDS", ceo: "雷電将軍" },
@@ -27,13 +66,9 @@ const labels = {
     "EPICLESES": { name: "EPICLESES PROMOTION", ceo: "フリーナ" }
 };
 
-// アーティストデータ
 const allGroups = {
     "gen1us": {
-        name: "GEN1US",
-        concept: "MASQUERADE",
-        layout: 3,
-        banner: "images/gen1us/masquerade-gen1us.jpg",
+        name: "GEN1US", concept: "MASQUERADE", layout: 3, banner: "images/gen1us/masquerade-gen1us.jpg",
         members: [
             { name: "Diluc", div: "Mondstadt", pos: "Main Vocal / Sub Dancer", label: "FAVONIUS", img: "images/gen1us/masquerade-diluc.jpg" },
             { name: "Zhongli", div: "Liyue", pos: "Lead Rapper / Sub Vocal", label: "QIXING", img: "images/gen1us/masquerade-zhongli.jpg" },
@@ -48,10 +83,7 @@ const allGroups = {
         discography: []
     },
     "4ggravate": {
-        name: "4ggravate",
-        concept: "CYBER / HIPHOP",
-        layout: 4,
-        banner: "images/4ggravate/4ggravate.jpg",
+        name: "4ggravate", concept: "CYBER / HIPHOP", layout: 4, banner: "images/4ggravate/4ggravate.jpg",
         members: [
             { name: "Alhaitham", div: "Sumeru", pos: "Main Dancer / Lead Rapper", label: "AKASHA", img: "images/4ggravate/cyber-alhaitham.jpg" },
             { name: "Cyno", div: "Sumeru", pos: "Main Rapper", label: "AKASHA", img: "images/4ggravate/cyber-cyno.jpg" },
@@ -63,10 +95,7 @@ const allGroups = {
         discography: [{ title: "All Out", type: "Mini Album", embed: `<iframe src="https://u.pcloud.link/publink/show?code=0ZfjiU5ZFUGomJCKJ8XBS3YOVjqAVYqbB2EX&view=list" width="100%" height="500" frameborder="0"></iframe>` }]
     },
     "6reeze": {
-        name: "6reeze",
-        concept: "EMPATHY",
-        layout: 3,
-        banner: "images/6reeze/6reeze.jpg",
+        name: "6reeze", concept: "EMPATHY", layout: 3, banner: "images/6reeze/6reeze.jpg",
         members: [
             { name: "Wanderer", div: "Inazuma", pos: "Main Dancer / Sub Vocal / Sub Rapper", label: "AKASHA", img: "images/6reeze/heart-wanderer.jpg" },
             { name: "Xiao", div: "Liyue", pos: "Main Rapper / Sub Vocal", label: "QIXING", img: "images/6reeze/heart-xiao.jpg" },
@@ -78,10 +107,7 @@ const allGroups = {
         discography: [{ title: "EMPATHY", type: "Mini Album", embed: `<iframe src="https://u.pcloud.link/publink/show?code=0ZKSiU5ZyeY0Tie17AfQ2saxTO7PBpiKqcC7&view=list" width="100%" height="500" frameborder="0"></iframe>` }]
     },
     "universe": {
-        name: "TVT U",
-        concept: "HANA",
-        layout: 4,
-        banner: "images/universe/tvt-u.jpg",
+        name: "TVT U", concept: "HANA", layout: 4, banner: "images/universe/tvt-u.jpg",
         members: [
             { name: "Gaming", div: "Liyue", pos: "Main Vocal", label: "QIXING", img: "images/universe/universe-gaming.jpg" },
             { name: "Chongyun", div: "Liyue", pos: "Lead Rapper", label: "QIXING", img: "images/universe/universe-chongyun.jpg" },
@@ -94,10 +120,7 @@ const allGroups = {
         discography: [{ title: "HANA", type: "Mini Album", embed: `<iframe src="https://u.pcloud.link/publink/show?code=0ZpJiU5ZTiglKk3h3KXhuoUPRUSWTL7UJ5Ok&view=list" width="100%" height="500" frameborder="0"></iframe>` }]
     },
     "hoth": {
-        name: "House of the Hearth",
-        concept: "EPICLESES",
-        layout: 4,
-        banner: "",
+        name: "House of the Hearth", concept: "EPICLESES", layout: 4, banner: "",
         members: [
             { name: "Neuvillette", div: "Fontaine", pos: "Main Vocal", label: "EPICLESES", isLeader: true, img: "" },
             { name: "Wriothesley", div: "Fontaine", pos: "Main Rapper", label: "EPICLESES", img: "" },
