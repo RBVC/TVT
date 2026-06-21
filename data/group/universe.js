741af20a157115ceb26da266f659d9569b63cc71
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-21 17:43:39 +0900

    Add files via upload

---
+++ data/group/universe.js
@@ -0,0 +1,22 @@
+/* --- START OF FILE data/group/universe.js --- */
+window.allGroups = window.allGroups || {};
+window.allGroups["universe"] = {
+    name: "TVT U",
+    concept: "HANA",
+    layout: 4,
+    basePath: "images/group/universe/",
+    banner: "tvt-u.jpg",
+    description: "Teyvat Entertainmentの育成機関「TVT Jr.」に所属する、選りすぐりの精鋭たちで構成された特別ユニット。事務所の次世代を担うスターを輩出すべく、ジンとリサが主導となり、Qixing Studioの夜蘭、Narukami Recordsの心海という敏腕マネージャー陣の協力を仰いでメンバーが選抜された。\n初期メンバーであるベネット、レザー、重雲、行秋の4名は、その圧倒的な支持率から後に「TVT DREAM」としてユニットデビューが決定。現在はアルベド、ゴロー、嘉明を加えた7名体制で活動しており、正式デビュー前ながら高い完成度を誇る。\nNCT DREAM、ENHYPEN、Dreamcatcher、HANAをロールモデルとし、儚くダークな世界観を軸に多種多様な楽曲をこなす。最大の特徴は楽曲のコンセプトに合わせて参加メンバーが入れ替わる流動的なシステムにあり、担当パートに縛られない挑戦的なスタイルと、ジュニアメンバーならではのエネルギッシュなダンスパフォーマンスを武器としている。",
+    members: [
+        { name: "Gaming", div: "Liyue", pos: "Main Vocal", label: "QIXING", img: "universe-gaming.jpg", subUnit: "-", remarks: "TVT Jr. 精鋭部隊" },
+        { name: "Chongyun", div: "Liyue", pos: "Lead Rapper", label: "QIXING", img: "universe-chongyun.jpg", subUnit: "TVT DREAM", remarks: "TVT Jr. 精鋭部隊" },
+        { name: "Xingqiu", div: "Liyue", pos: "Leader / Main Vocal", label: "QIXING", isLeader: true, img: "universe-xingqiu.jpg", subUnit: "TVT DREAM", remarks: "TVT Jr. 精鋭部隊" },
+        { name: "Albedo", div: "Mondstadt", pos: "Sub Vocal", label: "FAVONIUS", img: "universe-albedo.jpg", subUnit: "-", remarks: "TVT Jr. 精鋭部隊" },
+        { name: "Gorou", div: "Inazuma", pos: "Lead Dancer", label: "NARUKAMI", img: "universe-gorou.jpg", subUnit: "-", remarks: "TVT Jr. 精鋭部隊" },
+        { name: "Bennett", div: "Mondstadt", pos: "Main Dancer", label: "FAVONIUS", img: "universe-bennett.jpg", subUnit: "TVT DREAM", remarks: "TVT Jr. 精鋭部隊" },
+        { name: "Razor", div: "Mondstadt", pos: "Main Rapper", label: "FAVONIUS", img: "universe-razor.jpg", subUnit: "TVT DREAM", remarks: "TVT Jr. 精鋭部隊" }
+    ],
+    discography: [
+        { title: "HANA", type: "Mini Album", embed: `<iframe src="https://u.pcloud.link/publink/show?code=0ZpJiU5ZTiglKk3h3KXhuoUPRUSWTL7UJ5Ok&view=list" width="100%" height="500" frameborder="0"></iframe>` }
+    ]
+};
