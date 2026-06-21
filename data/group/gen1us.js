3bc5a1b7629a884457418e60b172a3e446daff47
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-21 16:35:17 +0900

    up

--- data/group/gen1us.js
+++ data/group/gen1us.js
@@ -6,7 +6,7 @@ window.allGroups["gen1us"] = {
     layout: 3,
     basePath: "images/group/gen1us/masquerade/",
     banner: "main.jpg",
-    description: "Teyvat Entertainment初の男性アイドルグループを作るため企画された「レーベル合同サバイバルオーディション番組『HEAVENTEEN』」の合格者9人によって結成されたスーパーユニット。デビュー前から俳優、モデル、小説家、アーティスト、インフルエンサーとして各界の第一線で活躍していた多種多様なプロフェッショナル集団であることが最大の特徴。パフォーマンス面では、ONEUSやSF9をロールモデルとしたクールかつラグジュアリーな楽曲を得意とし、高い歌唱力と9人のシンクロダンスで圧倒的なクオリティを誇る。\nEXO、XGをロールモデルとし、番組最終選考時のチーム編成をそのまま継承した派生ユニット「DCKZ」「BADIT」が存在しており、現在もコンセプトの異なるパフォーマンスを展開している。",
+    description: "Teyvat Entertainment初の男性アイドルグループを作るため企画された「レーベル合同サバイバルオーディション番組『HEAVENTEEN』」の合格者9人によって結成されたスーパーユニット。\nデビュー前から俳優、モデル、小説家、アーティスト、インフルエンサーとして各界の第一線で活躍していた多種多様なプロフェッショナル集団であることが最大の特徴。\nパフォーマンス面では、EXOやSF9、XGをロールモデルとしたクールかつラグジュアリーな楽曲を得意とし、高い歌唱力と9人のシンクロダンスで圧倒的なクオリティを誇る。\nONEUSをロールモデルとし、番組最終選考時のチーム編成をそのまま継承した派生ユニット「DCKZ」「BADIT」が存在しており、現在もコンセプトの異なるパフォーマンスを展開している。",
     members: [
         { name: "Diluc", div: "Mondstadt", pos: "Main Vocal / Sub Dancer", label: "FAVONIUS", img: "diluc.jpg", subUnit: "DCKZ", remarks: "アーティスト（Kaeyaとユニット活動中）" },
         { name: "Zhongli", div: "Liyue", pos: "Lead Rapper / Sub Vocal", label: "QIXING", img: "zhongli.jpg", subUnit: "DCKZ (リーダー)", remarks: "モデル・小説家" },
