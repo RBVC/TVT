29a22bc7b66fee61affa35dacad7b437a015ff60
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-21 17:18:56 +0900

    up

--- data/group/6reeze.js
+++ data/group/6reeze.js
@@ -6,13 +6,13 @@ window.allGroups["6reeze"] = {
     layout: 3,
     basePath: "images/group/6reeze/",
     banner: "6reeze.jpg",
-    description: "Teyvat Entertainmentが次世代のスターを育成すべく設立した下部組織「TVT Jr.（ティーブイティー・ジュニア）」から、初めてデビューを飾った6人組男性アイドルグループ。\nQixing StudioのCEOである凝光と、本グループのマネージャーに就任した蛍が「次世代アイドルの発掘」を掲げて『GEN1US』のメンバーのもとで練習生として活動していたウェンティ・魈・万葉に平蔵を加え結成。その後、スメールより帰国した空と放浪者が合流し現在の体制となった。Jr.時代から先輩グループのコンテンツに露出し、デビュー前から高い知名度を誇る。\nIVE、KiiiKiii、MADEINをロールモデルとし、ポップで愛らしい魅力からクールな存在感まで、楽曲のごとに別人のような変貌を遂げる「表情管理」の高さが最大の特徴。",
+    description: "Teyvat EntertainmentのGEN1USに続き二例目となる「レーベル複合型」の6人組男性アイドルグループ。\nQixing StudioのCEOである凝光と、本グループのマネージャーに就任した蛍が「次世代アイドルの発掘」を掲げて『GEN1US』のメンバーのもとで練習生として活動していたウェンティ・魈・万葉に加え、卓越したポテンシャルを持つ平蔵が加入。さらにマネージャー蛍の招集によってスメール留学から帰国した空と放浪者が合流。所属レーベルの垣根を超えた結束力を持つ現在の6名体制が確立された。\nIVE、KiiiKiii、MADEINをロールモデルとし、ポップで愛らしい魅力からクールな存在感まで、楽曲の世界観に合わせて別人のような変貌を遂げる「表情管理」の高さが最大の特徴。多様な感性が共鳴し合う、変幻自在なパフォーマンスを展開する。",
     members: [
-        { name: "Heizou", div: "Inazuma", pos: "Lead Rapper / Lead Dancer", label: "NARUKAMI", img: "heart-heizou.jpg", remarks: "TVT Jr.出身" },
+        { name: "Heizou", div: "Inazuma", pos: "Lead Rapper / Lead Dancer", label: "NARUKAMI", img: "heart-heizou.jpg", remarks: "" },
         { name: "Aether", div: "???", pos: "Main Vocal / Lead Dancer", label: "HQ", isLeader: true, img: "heart-aether.jpg", remarks: "4ggravateの元メンバー" },
-        { name: "Xiao", div: "Liyue", pos: "Main Rapper / Sub Vocal", label: "QIXING", img: "heart-xiao.jpg", remarks: "TVT Jr.出身" },
-        { name: "Venti", div: "Mondstadt", pos: "Center / Main Vocal", label: "FAVONIUS", img: "heart-venti.jpg", remarks: "TVT Jr.出身" },
-        { name: "Kazuha", div: "Inazuma", pos: "Main Vocal / Lead Dancer", label: "NARUKAMI", img: "heart-kazuha.jpg", remarks: "TVT Jr.出身" },
+        { name: "Xiao", div: "Liyue", pos: "Main Rapper / Sub Vocal", label: "QIXING", img: "heart-xiao.jpg", remarks: "" },
+        { name: "Venti", div: "Mondstadt", pos: "Center / Main Vocal", label: "FAVONIUS", img: "heart-venti.jpg", remarks: "" },
+        { name: "Kazuha", div: "Inazuma", pos: "Main Vocal / Lead Dancer", label: "NARUKAMI", img: "heart-kazuha.jpg", remarks: "" },
         { name: "Wanderer", div: "Inazuma", pos: "Main Dancer / Sub Vocal / Sub Rapper", label: "AKASHA", img: "heart-wanderer.jpg", remarks: "4ggravateの元メンバー" }
     ]
 };
