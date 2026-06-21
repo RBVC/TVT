3efbc531517a9696d32d76bdab6a1261f1bd5e4c
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-21 13:48:13 +0900

    Add files via upload

---
+++ data/group/4ggravate.js
@@ -0,0 +1,33 @@
+/* --- START OF FILE data/group/4ggravate.js --- */
+window.allGroups = window.allGroups || {};
+window.allGroups["4ggravate"] = {
+    name: "4ggravate",
+    concept: "CYBER / HIPHOP",
+    layout: 4,
+    basePath: "images/group/4ggravate/",
+    banner: "4ggravate.jpg",
+    description: "Teyvat Entertainment傘下のAkasha Labに所属する4人組男性アイドルグループ。グループ結成の経緯は、レーベルCEOであるナヒーダが、番組『HEAVENTEEN』候補生であったアルハイゼンとカーヴェ、そして新たにレーベルへ加入したセノとティナリを一堂に集めたことに端を発する。この大胆な人材確保は、業界内で俗に「ナヒーダ引き抜き事件」と称された。デビュー準備期間中には、パフォーマンス留学中であった空と、ナヒーダに拾われたスカラマシュ（現・放浪者）も練習生として参加していたが、両名が別プロジェクト『6reeze』としてデビューすることが決定したため、最終的に現在の4名体制へと至った。先行して絶大な人気を誇っていた『GEN1US』に対し、4ggravateはスメール地域での盤石な支持基盤を築く戦略をとり、その後の世界デビューにおいて爆発的なヒットを記録。aespaやK/DA、MAVEらをロールモデルとしたパワフルかつサイバーな楽曲を得意とし、卓越したダンススキルと、メンバー全員がラップをこなす楽曲構成を最大の武器としている。",
+    members: [
+        { name: "Alhaitham", div: "Sumeru", pos: "Main Dancer / Lead Rapper", label: "AKASHA", img: "cyber-alhaitham.jpg", subUnit: "-", remarks: "元教令院書記官" },
+        { name: "Cyno", div: "Sumeru", pos: "Main Rapper", label: "AKASHA", img: "cyber-cyno.jpg", subUnit: "-", remarks: "風紀官" },
+        { name: "Kaveh", div: "Sumeru", pos: "Lead Dancer / Lead Vocal", label: "AKASHA", img: "cyber-kaveh.jpg", subUnit: "-", remarks: "建築家" },
+        { name: "Tighnari", div: "Sumeru", pos: "Main Vocal", label: "AKASHA", isLeader: true, img: "cyber-tighnari.jpg", subUnit: "-", remarks: "レンジャー長" },
+        { name: "Aether", div: "???", pos: "Main Vocal / Lead Dancer", label: "HQ", isFormer: true, subUnit: "-", remarks: "練習生（現 6reeze）" },
+        { name: "Wanderer", div: "Inazuma", pos: "Main Dancer / Sub Vocal", label: "AKASHA", isFormer: true, subUnit: "-", remarks: "練習生（現 6reeze）" }
+    ],
+    confidential: {
+        ageTeams: [
+            { name: "年長チーム", members: "アルハイゼン・ティナリ" },
+            { name: "年中チーム", members: "セノ・カーヴェ" }
+        ],
+        couplings: ["セノ × ティナリ", "アルハイゼン × カーヴェ"],
+        ratios: {
+            vocal: "ティナリ ⇒ カーヴェ ⇒ アルハイゼン ⇒ セノ",
+            rap: "セノ ⇒ アルハイゼン ⇒ カーヴェ ⇒ ティナリ",
+            dance: "アルハイゼン ⇒ セノ ⇒ カーヴェ ⇒ ティナリ"
+        }
+    },
+    discography: [
+        { title: "All Out", type: "Mini Album", embed: `<iframe src="https://u.pcloud.link/publink/show?code=0ZfjiU5ZFUGomJCKJ8XBS3YOVjqAVYqbB2EX&view=list" width="100%" height="500" frameborder="0"></iframe>` }
+    ]
+};
