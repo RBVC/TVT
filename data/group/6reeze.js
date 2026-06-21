4c8b627c069520d096111a20626a6534feab6414
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-21 15:52:12 +0900

    Add files via upload

---
+++ data/group/6reeze.js
@@ -0,0 +1,34 @@
+/* --- START OF FILE data/group/6reeze.js --- */
+window.allGroups = window.allGroups || {};
+window.allGroups["6reeze"] = {
+    name: "6reeze",
+    concept: "EMPATHY",
+    layout: 3,
+    basePath: "images/group/6reeze/",
+    banner: "6reeze.jpg",
+    description: "Teyvat Entertainmentが次世代のスターを育成すべく設立した下部組織「TVT Jr.（ティーブイティー・ジュニア）」から、初めてデビューを飾った6人組男性アイドルグループ。QixingのCEO凝光とマネージャー蛍が、GEN1USのもとで研鑽を積んだウェンティ・魈・万葉に平蔵を加え結成。スメールより帰国した空と放浪者が合流し現在の体制となった。Jr.時代から先輩グループのコンテンツに露出し、デビュー前から高い知名度を誇る。IVE、KiiiKiii、MADEINをロールモデルとし、フレッシュな愛嬌や楽曲ごとに別人のような変貌を遂げる「表情管理」の高さが最大の特徴。",
+    members: [
+        { name: "Wanderer", div: "Inazuma", pos: "Main Dancer / Sub Vocal / Sub Rapper", label: "AKASHA", img: "heart-wanderer.jpg", subUnit: "-", remarks: "4ggravateの元メンバー" },
+        { name: "Xiao", div: "Liyue", pos: "Main Rapper / Sub Vocal", label: "QIXING", img: "heart-xiao.jpg", subUnit: "-", remarks: "TVT Jr.出身" },
+        { name: "Venti", div: "Mondstadt", pos: "Main Vocal / Center", label: "FAVONIUS", img: "heart-venti.jpg", subUnit: "-", remarks: "TVT Jr.出身" },
+        { name: "Kazuha", div: "Inazuma", pos: "Main Vocal / Lead Dancer", label: "NARUKAMI", img: "heart-kazuha.jpg", subUnit: "-", remarks: "TVT Jr.出身" },
+        { name: "Aether", div: "???", pos: "Main Vocal / Lead Dancer", label: "HQ", isLeader: true, img: "heart-aether.jpg", subUnit: "-", remarks: "4ggravateの元メンバー" },
+        { name: "Heizou", div: "Inazuma", pos: "Lead Rapper / Lead Dancer", label: "NARUKAMI", img: "heart-heizou.jpg", subUnit: "-", remarks: "TVT Jr.出身" }
+    ],
+    confidential: {
+        ageTeams: [
+            { name: "年長チーム", members: "放浪者・空" },
+            { name: "年中チーム", members: "万葉・平蔵" },
+            { name: "年少チーム", members: "ウェンティ・魈" }
+        ],
+        couplings: ["空 × 放浪者", "ウェンティ × 魈", "万葉 × 平蔵"],
+        ratios: {
+            vocal: "ウェンティ ⇒ 万葉 ⇒ 空 ⇒ 魈 ⇒ 平蔵 ⇒ 放浪者",
+            rap: "魈 ⇒ 平蔵 ⇒ 放浪者 ⇒ 万葉 ⇒ 空 ⇒ ウェンティ",
+            dance: "放浪者 ⇒ 万葉 ⇒ 平蔵 ⇒ 空 ⇒ 魈 ⇒ ウェンティ"
+        }
+    },
+    discography: [
+        { title: "EMPATHY", type: "Mini Album", embed: `<iframe src="https://u.pcloud.link/publink/show?code=0ZKSiU5ZyeY0Tie17AfQ2saxTO7PBpiKqcC7&view=list" width="100%" height="500" frameborder="0"></iframe>` }
+    ]
+};
