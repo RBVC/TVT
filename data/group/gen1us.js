ea3938f2e08c3212e4baab4dcf5488ae864d7313
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-21 13:25:09 +0900

    UP

--- data/group/gen1us.js
+++ data/group/gen1us.js
@@ -6,9 +6,12 @@ window.allGroups["gen1us"] = {
     layout: 3,
     basePath: "images/group/gen1us/",
     banner: "main.jpg",
-    // 💡 Wiki用の詳細解説
-    description: "Teyvat Entertainment所属の9人組男性アイドルグループ。レーベル合同サバイバル番組『HEAVENTEEN』の合格者によって結成された。メンバーはモンド・璃月・稲妻の各ディビジョンから選抜されており、デビュー前から俳優、モデル、小説家、アーティスト、インフルエンサーとして各界の第一線で活躍していた多種多様なプロフェッショナル集団であることが最大の特徴。パフォーマンス面では、ONEUSやSF9をロールモデルとしたクールかつラグジュアリーな楽曲を得意とし、高い歌唱力と9人のシンクロダンスで圧倒的なクオリティを誇る。グループ内には、番組最終選考時のチーム編成をそのまま継承した派生ユニット「DCKZ」「BADIT」が存在しており、現在もコンセプトの異なるパフォーマンスを展開している。",
-    units: ["DCKZ", "BADIT"],
+    description: "Teyvat Entertainment所属の9人組男性アイドルグループ。レーベル合同サバイバル番組『HEAVENTEEN』の合格者によって結成された。メンバーはモンド・璃月・稲妻の各ディビジョンから選抜されており、デビュー前から俳優、モデル、小説家、アーティスト、インフルエンサーとして各界の第一線で活躍していた多種多様なプロフェッショナル集団であることが最大の特徴。パフォーマンス面では、ONEUSやSF9をロールモデルとしたクールかつラグジュアリーな楽曲を得意とし、高い歌唱力と9人のシンクロダンスで圧倒的なクオリティを誇る。",
+    // 💡 ユニット情報をテーブル化するために詳細化
+    units: [
+        { name: "DCKZ", concept: "攻撃的なサウンドが特徴のビジュアル最強集団" },
+        { name: "BADIT", concept: "アジアンテイストなサウンドが特徴の実力派集団" }
+    ],
     members: [
         { name: "Diluc", div: "Mondstadt", pos: "Main Vocal / Lead Dancer", label: "FAVONIUS", img: "diluc.jpg", remarks: "アーティスト（Kaeyaとユニット活動中）" },
         { name: "Zhongli", div: "Liyue", pos: "Lead Rapper / Sub Vocal", label: "QIXING", img: "zhongli.jpg", remarks: "モデル・小説家" },
@@ -20,7 +23,6 @@ window.allGroups["gen1us"] = {
         { name: "Dainsleif", div: "Mondstadt", pos: "Main Dancer / Sub Rapper", label: "FAVONIUS", img: "dainsleif.jpg", remarks: "俳優" },
         { name: "Thoma", div: "Inazuma", pos: "Lead Dancer / Sub Vocal", label: "NARUKAMI", img: "thoma.jpg", remarks: "アクション俳優" }
     ],
-    // 💡 内部機密資料セクション用データ
     confidential: {
         ageTeams: [
             { name: "年長チーム", members: "ダイン・鍾離・白朮" },
