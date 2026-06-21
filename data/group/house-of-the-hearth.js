bedf2d1a1054a5bfb3080562ce888fe117405213
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-21 19:02:22 +0900

    up

--- data/group/house-of-the-hearth.js
+++ data/group/house-of-the-hearth.js
@@ -8,9 +8,9 @@ window.allGroups["house-of-the-hearth"] = {
     banner: "main.jpg",
     description: "Teyvat Entertainment傘下のEpicleses Promotionに所属する4人組男性アイドルグループ。\nオーディション番組『HEAVENTEEN』の候補生として名を馳せたヌヴィレットとリオセスリは、番組終了後も強い信頼関係で結ばれた実力派ユニットとして精力的な活動を展開していた。その後、レーベルのCEOであるフリーナの仲介により、天性の華を持つリネと繊細な表現力を備えたフレミネの二名を紹介されたことで、現在の4名体制へと進化を遂げた。\nKISS OF LIFEやXLOVをロールモデルとし、ヌヴィレットの圧倒的な歌唱力を筆頭に、音楽の枠を飛び越えて一つの物語を鑑賞しているかのような、芸術性の高い独創的な世界観を構築しているのが最大の特徴。劇場を彷彿とさせるドラマチックな展開と、4人が織りなすミステリアスな空気感は、業界内でも唯一無二の輝きを放っている。",
     members: [
-        { name: "Neuvillette", div: "Fontaine", pos: "Main Vocal", label: "EPICLESES", isLeader: true, img: "neuvillette.jpg", remarks: "『HEAVENTEEN』候補生" },
         { name: "Wriothesley", div: "Fontaine", pos: "Main Rapper", label: "EPICLESES", img: "writhosley.jpg", remarks: "『HEAVENTEEN』候補生" },
         { name: "Lyney", div: "Fontaine", pos: "Main Dancer", label: "EPICLESES", img: "lyney.jpg", remarks: "" },
+        { name: "Neuvillette", div: "Fontaine", pos: "Main Vocal", label: "EPICLESES", isLeader: true, img: "neuvillette.jpg", remarks: "『HEAVENTEEN』候補生" },
         { name: "Freminet", div: "Fontaine", pos: "Lead Vocal", label: "EPICLESES", img: "freminet.jpg", remarks: "" }
     ]
 };
