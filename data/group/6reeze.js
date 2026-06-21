b0da78c8884a1292a2ac8922c87ca37a7f380d60
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-21 18:01:17 +0900

    UP

--- data/group/6reeze.js
+++ data/group/6reeze.js
@@ -9,7 +9,7 @@ window.allGroups["6reeze"] = {
     description: "Teyvat EntertainmentのGEN1USに続き二例目となる「レーベル複合型」の6人組男性アイドルグループ。\nQixing StudioのCEOである凝光と、本グループのマネージャーに就任した蛍が「次世代アイドルの発掘」を掲げて『GEN1US』のメンバーのもとで練習生として活動していたウェンティ・魈・万葉に加え、卓越したポテンシャルを持つ平蔵が加入。さらにマネージャー蛍の招集によってスメール留学から帰国した空と放浪者が合流。所属レーベルの垣根を超えた結束力を持つ現在の6名体制が確立された。\nIVE、KiiiKiii、MADEINをロールモデルとし、ポップで愛らしい魅力からクールな存在感まで、楽曲の世界観に合わせて別人のような変貌を遂げる「表情管理」の高さが最大の特徴。多様な感性が共鳴し合う、変幻自在なパフォーマンスを展開する。",
     members: [
         { name: "Heizou", div: "Inazuma", pos: "Lead Rapper / Lead Dancer", label: "NARUKAMI", img: "heart-heizou.jpg", remarks: "" },
-        { name: "Aether", div: "???", pos: "Main Vocal / Lead Dancer", label: "HQ", isLeader: true, img: "heart-aether.jpg", remarks: "4ggravateの元メンバー" },
+        { name: "Aether", div: "???", pos: "Lead Vocal", label: "HQ", isLeader: true, img: "heart-aether.jpg", remarks: "4ggravateの元メンバー" },
         { name: "Xiao", div: "Liyue", pos: "Main Rapper / Sub Vocal", label: "QIXING", img: "heart-xiao.jpg", remarks: "" },
         { name: "Venti", div: "Mondstadt", pos: "Center / Main Vocal", label: "FAVONIUS", img: "heart-venti.jpg", remarks: "" },
         { name: "Kazuha", div: "Inazuma", pos: "Main Vocal / Lead Dancer", label: "NARUKAMI", img: "heart-kazuha.jpg", remarks: "" },
